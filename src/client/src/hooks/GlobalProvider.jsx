f9d37ca33401f7457bec15d8dfc61a3b6112a334
thiagomsr20 <thiagomsr20@gmail.com>
2025-04-08 21:35:40 -0300

    feat: idioma e tema inicial com base na pref do browser do usuario

--- src/client/src/hooks/GlobalProvider.jsx
+++ src/client/src/hooks/GlobalProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const GlobalContext = createContext();
 
@@ -6,8 +6,18 @@ export function GlobalProvider({ children }) {
     const [language, setLanguage] = useState('pt');
     const [darkMode, setDarkTheme] = useState(false);
 
+    useEffect(() => {
+        // Detecta idioma do navegador
+        const userLang = navigator.language || navigator.userLanguage;
+        setLanguage(userLang.startsWith('pt') ? 'pt' : 'en');
+
+        // Detecta preferência de tema do sistema
+        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        setDarkTheme(prefersDark);
+    }, []);
+
     return (
-        <GlobalContext.Provider value={{ language, setLanguage, darkMode, setDarkTheme}}>
+        <GlobalContext.Provider value={{ language, setLanguage, darkMode, setDarkTheme }}>
             {children}
         </GlobalContext.Provider>
     );
